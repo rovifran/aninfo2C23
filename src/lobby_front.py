f9f5e400682cc55fae43ffdd8aee8838c66a49de
PandolfiJoaquin <jpandolfi@fi.uba.ar>
2023-12-15 11:38:33 -0300

    Refactor grafico/UX se agrego imagen y texto a los botones, tambien fondo

--- src/lobby_front.py
+++ src/lobby_front.py
@@ -1,31 +1,44 @@
 import pygame
 pygame.init()
-# pygame.font.init()
-# screen = pygame.display.set_mode((800, 600))
+
+fondo = pygame.image.load("img/fondo.jpg")
+img_boton = pygame.image.load("img/boton.png")
+
 
 #botonsito
 class Button:
-    def __init__(self, x, y, width, height, color, action):
+    def __init__(self, x, y, width, height, color, text, action):
         self.rect = pygame.Rect(x, y, width, height)
         self.color = color
         self.action = action
+        self.text = text
 
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
+    def draw(self, pygame_screen):
+        scaled_img = pygame.transform.scale(img_boton, (self.rect.width, self.rect.height))
+        pygame_screen.blit(scaled_img, self.rect)
+        # pygame.draw.rect(pygame_screen, self.color, self.rect)
+        font = pygame.font.Font(None, 36)
+        text = font.render(self.text, True, (0,0,0))
+        x = self.rect.x
+        y = self.rect.y
+        w = self.rect.width
+        h = self.rect.height
+        text_rect = pygame.Rect(x + w * 0.2, y + h * 0.2, w * 0.6, h * 0.6)
+        pygame_screen.blit(text, text_rect)
 
     def is_clicked(self, pos):
         return self.rect.collidepoint(pos)
 
-sin_flor = Button(100, 100, 200, 50, (255, 0, 0), lambda : (screen.set_flor(False), screen.start_match()))
-con_flor = Button(100, 250, 200, 50, (0, 255, 0), lambda: (screen.set_flor(True), screen.start_match()))
+sin_flor = Button(100, 100, 200, 50, (255, 0, 0), "Sin flor" ,lambda : (screen.set_flor(False), screen.start_match()))
+con_flor = Button(400, 100, 200, 50, (0, 255, 0), "Con flor", lambda: (screen.set_flor(True), screen.start_match()))
 widgets_for_screen_3 = [sin_flor, con_flor]
 
-quit_button = Button(100, 100, 200, 50, (255, 0, 0), lambda: (screen.stop(), pygame.quit()))
-popup_button = Button(400, 100, 200, 50, (0, 255, 0), lambda: play_screen(screen, "Enter a number:", 0, lambda x: print(f"You entered {x}")))
+quit_button = Button(100, 100, 200, 50, (255, 0, 0), "Salir" , lambda: (screen.stop(), pygame.quit()))
+popup_button = Button(400, 100, 200, 50, (0, 255, 0), "Jugar", lambda: play_screen(screen, "Enter a number:", 0, lambda x: print(f"You entered {x}")))
 widgets_for_screen_1 = [quit_button, popup_button]
 
-hasta_15 = Button(100, 100, 200, 50, (0, 255, 0), lambda: (screen.set_amt_players(15), screen.change_widgets(widgets_for_screen_3)))
-hasta_30 = Button(100, 250, 200, 50, (0, 255, 0), lambda: (screen.set_amt_players(30), screen.change_widgets(widgets_for_screen_3)))
+hasta_15 = Button(100, 100, 200, 50, (0, 255, 0), "a 15", lambda: (screen.set_amt_players(15), screen.change_widgets(widgets_for_screen_3)))
+hasta_30 = Button(400, 100, 200, 50, (0, 255, 0), "a 30", lambda: (screen.set_amt_players(30), screen.change_widgets(widgets_for_screen_3)))
 widgets_for_screen_2 = [hasta_15, hasta_30]
 
 
@@ -67,7 +80,9 @@ class Screen:
                 widget.action()
 
     def display(self):
-        self.pygame_screen.fill((0, 0, 0))
+        # self.pygame_screen.fill((0, 0, 0))
+        self.pygame_screen.blit(fondo, (0, 0))
+
         for widget in self.widgets:
             widget.draw(self.pygame_screen)
         pygame.display.flip()
@@ -108,3 +123,4 @@ if (screen.starting == True):
     print(screen.game_config)
 #    quit_button.draw(screen)
 #    popup_button.draw(screen)
+
